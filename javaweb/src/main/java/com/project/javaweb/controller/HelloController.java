5faaafd53afec490fb5a67a44f3ef034f00dd304
Jimmy <2489219080@qq.com>
2021-11-29 12:39:41 +0800

    first try

---
+++ javaweb/src/main/java/com/project/javaweb/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.project.javaweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class HelloController {
+    @RequestMapping
+    public String hello(Model model){
+        model.addAttribute("hello", "Hello,world!");
+        return "index";
+    }
+}
