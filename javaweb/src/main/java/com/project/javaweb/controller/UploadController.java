0d0ee9aee244d46d084e4b65c905036e82a4eb7d
Jimmy <2489219080@qq.com>
2021-12-04 17:27:29 +0800

    basic image upload

---
+++ javaweb/src/main/java/com/project/javaweb/controller/UploadController.java
@@ -0,0 +1,39 @@
+package com.project.javaweb.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@RestController
+public class UploadController {
+    @PostMapping("/upload/img")
+    public Map<String, Object> hello(@RequestParam("file[]") MultipartFile content) throws IllegalStateException, IOException{
+        String filename = content.getOriginalFilename();
+        String filepath = "img/"+filename;
+        File dest = new File(filepath);
+        Map<String,Object> info = new HashMap<>();
+        Map<String,Object> data = new HashMap<>();
+        Map<String,String> succMap = new HashMap<>();
+
+        succMap.put(filename, filepath);
+
+        data.put("errFiles","");
+        data.put("succMap", succMap);
+
+        info.put("msg","ok");
+        info.put("code", 0);
+        info.put("data", data);
+
+        content.transferTo(dest);
+        return info;
+    }
+}
